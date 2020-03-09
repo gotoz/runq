85a79c4d7e8031153a40673c9257fa6cf391b858
Peter Morjan <peter.morjan@de.ibm.com>
2020-03-09 10:10:18 +0000

    simplify runc patch

--- cmd/runq/dummy.go
+++
@@ -1,8 +0,0 @@
-// This file makes linter such as "go vet" happy.
-
-package main
-
-// validateProcessSpec is defined in the main package of runc.
-func validateProcessSpec(spec interface{}) error {
-	return nil
-}
